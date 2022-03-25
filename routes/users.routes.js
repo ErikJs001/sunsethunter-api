e415ad453ff381eec3035cef7378caeded3f3382
adrianluev <58182697+adrianluev@users.noreply.github.com>
2022-03-24 23:47:01 -0600

    Update users.routes.js

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,7 +2,8 @@ const { Router } = require('express');
 const { 
     addSite,
     getSites,
-    getSite
+    getSite,
+    removeSite
 } = require('../controllers/users.controller');
 
 const router = Router();
@@ -12,6 +13,7 @@ router.post('/add_site', addSite);
 router.get('/get_sites', getSites);
 
 router.get('/get_site', getSite);
+router.get('/remove_site', removeSite);
 
 
 module.exports = router;
